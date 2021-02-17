e76e2835334d0dd19284098690ce191c2764ebe8
Terransky <nodeathking@gmail.com>
2021-02-16 16:09:39 -0800

    got event handler to render new images and track votes, added button to display list of results.

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const rounds = 25;
 
 let imgArr = ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum", "chair", "cthulhu", "dog-duck", "dragon", "pen", "pet-sweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "usb", "water-can", "wine-glass"];
 
-console.log(imgArr); // remove later 
+
 
 let productArr = [];
 let imgCache = [];
@@ -30,6 +30,10 @@ let imgContainerGroup = document.querySelector("body main");
 let imgContainerOne = document.querySelector("img:first-child");
 let imgContainerTwo = document.querySelector("img:nth-child(2)");
 let imgContainerThree = document.querySelector("img:nth-child(3)");
+let button = document.querySelector("button");
+let uList = document.querySelector("ul");
+
+
 
 
 function Product (name, src) {
@@ -45,7 +49,7 @@ for (let i = 0; i < imgArr.length; i++) {
   productArr[i] = new Product (imgArr[i], `img/${imgArr[i]}.jpg`);
 }
 
-console.log(productArr); // remove later
+
 
 function randomInt() {
   return Math.floor(Math.random() * productArr.length); // referenced from MDN
@@ -61,14 +65,14 @@ function assignRandomImg() {
   imgTwo = productArr[randomInt()];
   imgThree = productArr[randomInt()];
 
-  console.log(randomInt());
+  
 
  while ((imgOne === imgTwo) || (imgOne === imgThree) || (imgTwo === imgThree)) { //will rewrite this later using prototype.includes
     assignRandomImg(); //recursion, run again from the top
   }
   
   imgCache.push(imgOne, imgTwo, imgThree);
-  console.log(imgOne, imgTwo, imgThree); // remove later
+  
   
 }
 
@@ -83,8 +87,14 @@ function renderImages() {
   for (let i in imgCache) {
     imgCache[i].views++;
   }
-  console.log(imgCache);
+  
 
+}
+
+function clearCache() {
+  for (let i = imgCache.length; i > 0; i--) {
+    imgCache.pop();    
+  }
 }
 
 
@@ -96,18 +106,36 @@ let totalClicks = 0;
 
 function handleClick(event) {
   totalClicks++;
-  let imgClicked = event.target.querySelector("img");
-
+  let imgClicked = event.target.getAttribute("src");
+  
+  
   for (let i in imgCache) {
     if (imgClicked === imgCache[i].src) {
       imgCache[i].votes++;
+      clearCache();
+      
+      renderImages();
     }
   }
-  renderImages();
+  
   if (totalClicks === rounds) {
     imgContainerGroup.removeEventListener('click', handleClick);
+    button.addEventListener('click', handleButton);
   }
 }
 
+function handleButton(event){
+    
+  for (let i in productArr) {
+    let li = document.createElement("li");
+    let temp = document.createTextNode(`${productArr[i].name} had ${productArr[i].votes} votes, and was seen ${productArr[i].views} times.`);
+    li.appendChild(temp);
+    uList.appendChild(li);
+  }
+  button.removeEventListener('click', handleButton);
+}
 
 imgContainerGroup.addEventListener('click', handleClick);
+button.addEventListener('click', handleButton);
+
+// after voting rounds, remove event listener and add button with text View Results which displays the list of all products followed by the votes and number of times viewed. e.g. "dog-duck had 3 votes, and was seen 5 times".
